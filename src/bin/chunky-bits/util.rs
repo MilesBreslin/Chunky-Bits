426ac93467e3afcaba567727dcfebf4801a429fb
Miles Breslin <milesbreslin@gmail.com>
2021-05-23 18:56:50 -0700

    Fix unsafety for writer being still used after the function exits

--- src/bin/chunky-bits/util.rs
+++ src/bin/chunky-bits/util.rs
@@ -25,31 +25,34 @@ where
     let writer: &'static mut W = unsafe { std::mem::transmute(writer) };
     let write_handle = tokio::spawn(async move {
         while let Some(data) = rx.recv().await {
-            writer.write_all(data).await.unwrap()
+            writer.write_all(data).await?;
         }
+        Ok::<_, anyhow::Error>(())
     });
+    let inner = async move {
+        let mut total_bytes: u64 = 0;
 
-    let mut total_bytes: u64 = 0;
-
-    loop {
-        let data: &mut [u8] = if last_used.eq(&data_b.as_ptr()) {
-            &mut data_a
-        } else {
-            &mut data_b
-        };
-        last_used = data.as_ptr();
-        let tx = tx.reserve().await?;
-        match reader.read(data).await? {
-            0 => break,
-            len => {
-                let data: &[u8] = &data[0..len];
-                let data: &'static [u8] = unsafe { std::mem::transmute(data) };
-                tx.send(data);
-                total_bytes += len as u64;
-            },
+        loop {
+            let data: &mut [u8] = if last_used.eq(&data_b.as_ptr()) {
+                &mut data_a
+            } else {
+                &mut data_b
+            };
+            last_used = data.as_ptr();
+            let tx = tx.reserve().await?;
+            match reader.read(data).await? {
+                0 => break,
+                len => {
+                    let data: &[u8] = &data[0..len];
+                    let data: &'static [u8] = unsafe { std::mem::transmute(data) };
+                    tx.send(data);
+                    total_bytes += len as u64;
+                },
+            }
         }
-    }
-    drop(tx);
-    write_handle.await.unwrap();
-    Ok(total_bytes)
+        drop(tx);
+        Ok(total_bytes)
+    };
+    write_handle.await??;
+    inner.await
 }
